b648aff867fcb3531c18bbe7b5b668e1d0f09a52
Recmars <magbutongrecmar829@gmail.com>
2025-02-23 09:57:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,13 +51,3 @@ if __name__ == "__main__":
 
 
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return {"message": "Hello, React Native!"}
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
