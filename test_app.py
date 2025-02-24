4bbab4525535b5c9ea8943927e767952f8b9f997
Recmars <magbutongrecmar829@gmail.com>
2025-02-24 20:53:13 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+import os #add this import
 
 app = FastAPI()
 
@@ -8,5 +9,10 @@ async def root():
     return {"message": "Hello from Render!"}
 
 if __name__ == "__main__":
-    print("Starting server on 0.0.0.0:8000")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    print("Starting server...")
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000)
+    server = uvicorn.Server(config)
+    print(f"Uvicorn host: {config.host}, port: {config.port}")
+    print(f"Current Working Directory: {os.getcwd()}") # added line.
+    print(f"Files in Current Directory: {os.listdir()}") # added line.
+    server.run()
