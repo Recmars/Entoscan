19cffe3ee63e4688588dcd067e7e0b73f0bab7e9
Recmars <magbutongrecmar829@gmail.com>
2025-02-25 15:08:36 +0800

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -6,10 +6,19 @@ def load_labels(label_file):
     with open(label_file, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
-def load_and_preprocess_image_keras(image_path, image_size):
+def load_and_preprocess_image(image_path, image_size):
     img = Image.open(image_path).convert('RGB')
     img = img.resize(image_size)
     img_array = np.array(img, dtype=np.float32)
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
     img_array = img_array / 255.0  # Normalize
     return img_array
+
+def classify_image(image_array, interpreter):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], image_array)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data[0]
